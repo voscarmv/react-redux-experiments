c319a2b2d8e530b0ac9b54bc617d87726624a582
Oscar <voscarmv@gmail.com>
2021-01-31 16:45:08 -0600

    Fixed eslint

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 
 const Square = () => {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={this.props.onClick}>
+      {this.props.value}
     </button>
   );
 };
